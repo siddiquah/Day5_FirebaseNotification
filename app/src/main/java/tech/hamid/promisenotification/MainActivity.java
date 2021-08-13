575f7be39bac75bb3f1708a948390bd15937ca71
siddiquah anjum <siddiquahanjum111@gmail.com>
2021-08-13 22:26:11 +0530

    initial commit

---
+++ app/src/main/java/tech/hamid/promisenotification/MainActivity.java
@@ -0,0 +1,17 @@
+package tech.hamid.promisenotification;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Remember , This notification is pushed from firebase, cloud messaging.
+
+    }
+}
